d3591425b7bf663c4d6314937ef160365b61b377
WaltDev29 <waltdev29@gmail.com>
2026-05-13 16:28:28 +0900

    # 260513 - 유전자 알고리즘 기반 식인종 선교사 문제 학습

---
+++ 260513/01_game_genetic.py
@@ -0,0 +1,106 @@
+import random
+import copy
+from game import play_game
+
+POPULATION_SIZE = 50  # 개체 집단의 크기
+TOURNAMENT_SIZE = 3  # 토너먼트 선택에서 경쟁하는 개체 수
+MUTATION_RATE = 0.1  # 돌연 변이 확률
+SIZE = 15  # 하나의 유전자에서 염색체 개수
+GENERATION = 300  # 세대 수
+
+
+# 염색체를 클래스로 정의한다.
+class Chromosome:
+    command_list = [[1, 0], [2, 0], [0, 1], [0, 2], [1, 1]]
+
+    def __init__(self, g=None):
+        self.genes = [] if g is None else copy.deepcopy(g)  # 염색체는 리스트로 구현된다.
+        self.fitness = 0  # 적합도
+        if self.genes.__len__() == 0:  # 염색체가 초기 상태이면 초기화한다.
+            for i in range(SIZE):
+                self.genes.append(random.choice(Chromosome.command_list))
+
+    def cal_fitness(self):  # 적합도를 계산한다.
+        self.fitness = 0;
+        self.fitness, self.count = play_game(copy.deepcopy(self.genes))
+        return self.fitness
+
+    def __str__(self):
+        return self.genes.__str__()
+
+
+# 염색체와 적합도를 출력한다.
+def print_p(pop):
+    i = 0
+    for x in pop:
+        print("염색체 #", i, "=", x, "적합도=", x.fitness, "시도 횟수=", x.count)
+        i += 1
+    print("")
+
+
+# 선택 연산
+def select(pop):
+    tournament = random.sample(pop, TOURNAMENT_SIZE)
+    tournament.sort(key=lambda x: x.fitness, reverse=True)
+    return tournament[0]
+
+
+# 교차 연산
+def crossover(pop):
+    father = select(pop)
+    mother = select(pop)
+
+    while mother == father:
+        mother = select(pop)
+
+    index = random.randint(1, SIZE - 1)
+    child1 = father.genes[:index] + mother.genes[index:]
+    child2 = mother.genes[:index] + father.genes[index:]
+    return (child1, child2)
+
+
+# 돌연변이 연산
+def mutate(c):
+    for i in range(SIZE):
+        if random.random() < MUTATION_RATE:
+            c.genes[i] = random.choice(Chromosome.command_list)
+
+
+# 메인 프로그램
+population = []
+
+# 초기 염색체를 생성하여 객체 집단에 추가한다.
+for i in range(POPULATION_SIZE):
+    population.append(Chromosome())
+
+count = 0
+population.sort(key=lambda x: x.cal_fitness(), reverse=True)
+print("세대 번호=", count)
+print_p(population)
+count = 1
+
+
+for i in range(GENERATION):
+    new_pop = []
+
+    best = population[0]
+    new_pop.append(best)
+
+    # 선택과 교차 연산
+    for _ in range((POPULATION_SIZE // 2) - 1):
+        c1, c2 = crossover(population)
+        new_pop.append(Chromosome(c1))
+        new_pop.append(Chromosome(c2))
+
+    # 자식 세대가 부모 세대를 대체한다.
+    # 깊은 복사를 수행한다.
+    population = new_pop.copy()
+
+    # 돌연변이 연산
+    for c in population[1:]: mutate(c)
+
+    # 출력을 위한 정렬
+    population.sort(key=lambda x: x.cal_fitness(), reverse=True)
+    print("세대 번호=", count)
+    print_p(population)
+    count += 1
---
+++ 260513/game.py
@@ -0,0 +1,105 @@
+def play_game(command:list[list[int]]):
+    CHANCE = 20
+    TOTAL = 3
+    M = 0
+    C = 1
+    ground = [[0,0], [TOTAL, TOTAL]]
+    boat_side = 1 # 0: left, 1: right
+    side_str = ["왼쪽", "오른쪽"]
+
+    is_game_over = False
+    is_clear = False
+
+    # print("\n\n============ 게임 시작! ============")
+    count = 0
+    while True:
+        count += 1
+        if count > CHANCE or len(command) == 0:
+            is_game_over = True
+            break
+        # ============ 상태 출력 및 입력 ============
+        # print(f"선교사 {ground[0][M]} | 식인종 {ground[0][C]}\t\t선교사 {ground[1][M]} | 식인종 {ground[1][C]}")
+        # print(f"배 위치: {side_str[boat_side]}")
+
+        onboarding = command.pop(0)
+        # print(f"입력: {onboarding}\n")
+
+
+
+        # ============ 예외 처리 ============
+        if len(onboarding) != 2 or sum(onboarding) > 2:
+            # print("최대 두 명까지 탑승할 수 있습니다. (예: 1 1)")
+            continue
+
+        if onboarding[0] + onboarding[1] <= 0:
+            # print("최소 한 명은 배에 탑승해야 합니다.")
+            continue
+
+        if onboarding[0] < 0 or onboarding[1] < 0:
+            # print("음수 입력은 불가능합니다.")
+            continue
+
+        if onboarding[M] > ground[boat_side][M] or onboarding[C] > ground[boat_side][C]:
+            # print(f"{side_str[boat_side]}에 해당 인원이 없습니다.")
+            continue
+
+
+
+        # ============ 탑승 ============
+        ground[boat_side][M] -= onboarding[M]
+        ground[boat_side][C] -= onboarding[C]
+        boat_side = abs(1 - boat_side)
+
+
+
+        # ============ 이동 ============
+        ground[boat_side][M] += onboarding[M]
+        ground[boat_side][C] += onboarding[C]
+
+
+
+        # ============ 게임 오버 체크 ============
+        if (ground[0][M] > 0 and ground[0][M] < ground[0][C]) or (ground[1][M] > 0 and ground[1][M] < ground[1][C]):
+            is_game_over = True
+            break
+
+
+
+        # ============ 게임 클리어 체크 ============
+        if ground[0][M] == TOTAL and ground[0][C] == TOTAL:
+            is_clear = True
+            break
+
+
+    # ============ 결과 출력 ============
+    if is_game_over:
+        if count > CHANCE:
+            # print(f"\n###### 게임 오버! {CHANCE}회 이상 시도했습니다. ######")
+            pass
+        else:
+            # print("\n###### 게임 오버! 선교사가 잡아먹혔습니다. ######")
+            pass
+    elif is_clear:
+        # print("\n###### 게임 클리어! 모두 안전하게 건넜습니다. ######")
+        pass
+
+    # print(f"------ 최종 상태 ------\n선교사 {ground[0][M]} | 식인종 {ground[0][C]}\t\t선교사 {ground[1][M]} | 식인종 {ground[1][C]}")
+
+
+    fitness = 0
+
+    # 왼쪽으로 이동한 사람 수
+    fitness += (ground[0][M] + ground[0][C]) * 100
+
+    # 이동 횟수 패널티
+    fitness -= count * 3
+
+    # 게임오버 패널티
+    if is_game_over:
+        fitness -= 50
+
+    # 클리어 보너스
+    if is_clear:
+        fitness += 5000
+
+    return fitness, count
